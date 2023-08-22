408eb77b87e6ee9843048ee4bcf51b71458557b5
rilwan <salawudeenrilwan3@gmail.com>
2023-08-22 13:59:26 +0200

    Building the Game Card update

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,8 +10,9 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
-      <CardBody></CardBody>
-      <Heading fontSize="2xl">{game.name}</Heading>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
     </Card>
   );
 };
